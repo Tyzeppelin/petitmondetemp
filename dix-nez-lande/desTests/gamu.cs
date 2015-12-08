c954bb3dd16a69fd923a44058fe4950f43696c5f
Tyzeppelin <boschet.francois@gmail.com>
2015-12-08 01:18:56 +0100

    commencé les tests mais visual casse les noix avec les import et les parametres et les using ...

---
+++ dix-nez-lande/desTests/gamu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dix_nez_lande.Implem.;
+
+namespace desTests { }
+
+{
+    [TestClass]
+    public class Gametest
+    {
+        [TestMethod]
+        public void TestBuilder(int SizeMap)
+        {
+            GameBuilder g;
+            g.create().board(GameBuilder.LitMap).player1("francois", "humain").player2("aurelien", "orc");
+
+            //tests g quand j'aurai l'autocompletion
+
+
+
+        }
+    }
+}
