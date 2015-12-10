2106e642b5e136d38c001d510eb5ef07c757bac9
Aurélien Fontaine <aurelien.fontaine@insa-rennes.fr>
2015-12-10 22:17:04 +0100

    algo fix

--- dix-nez-lande/libCPP/Algo.cpp
+++ dix-nez-lande/libCPP/Algo.cpp
@@ -10,8 +10,14 @@ void Algo::fillMap(int map[], int size)
 {
 	srand(time(NULL));
 	int MapSize = size * size;
+	int nbType[4] = { MapSize / 4, MapSize / 4, MapSize / 4, MapSize / 4 };
 	for (int x = 0; x < MapSize; x++)
 	{
-			map[x] = x % 4;
+		int tile = rand() % 4;
+		while (nbType[tile] == 0) {
+			tile = rand() % 4;
+		}
+		nbType[tile]--;
+		map[x] = tile;
 	}
 }
