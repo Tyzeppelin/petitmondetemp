ad26f8d2888fb227e03bc85580b8fce9ebaad588
Tyzeppelin <boschet.francois@gmail.com>
2015-12-10 09:28:58 +0100

    on avance les tests.

--- dix-nez-lande/desTests/gamu.cs
+++ dix-nez-lande/desTests/gamu.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using dix_nez_lande.Implem.;
+using dix_nez_lande.Implem;
+using dix_nez_lande;
 
-namespace desTests { }
+namespace desTests
 
 {
     [TestClass]
@@ -11,13 +12,28 @@ namespace desTests { }
         [TestMethod]
         public void TestBuilder(int SizeMap)
         {
-            GameBuilder g;
-            g.create().board(GameBuilder.LitMap).player1("francois", "humain").player2("aurelien", "orc");
+            // INIT
+            GameBuilder g = GameBuilder.create();
+            g = g.board(GameBuilder.LitMap).player1("francois", "human").player2("aurelien", "orc");
+            Game tg = g.build();
+            
+            Player p1 = tg.players[0];
+            Player p2 = tg.players[1];
 
-            //tests g quand j'aurai l'autocompletion
+            //qui de mieux ue jordy et willy pour représenter les hommes et les orcs
+            Race jordy = RaceFactory.getRaceFactory().getRace("human");
+            Race willy = RaceFactory.getRaceFactory().getRace("orc");
 
+            // !! WARNING : FUN OVERFLOW !!
 
+            Assert.AreEqual(tg.map.size, GameBuilder.LitMap);
 
+            Assert.AreEqual(p1.name, "francois");
+            Assert.AreEqual(p2.name, "aurelien");
+
+            Assert.AreEqual(p1.race.name, jordy.name);
+            Assert.AreEqual(p2.race.name, willy.name);
+            
         }
     }
 }
